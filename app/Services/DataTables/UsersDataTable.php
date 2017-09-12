1a902002e644a1fd5b678525c8d1c05f18e232a3
Saulo Vinícius <saulovinicius987@gmail.com>
2017-09-12 20:29:59 -0300

    Resolvido o bug na tabela de usuários e na barra lateral de Médicos

--- app/Services/DataTables/UsersDataTable.php
+++ app/Services/DataTables/UsersDataTable.php
@@ -15,128 +15,125 @@ use Yajra\DataTables\Services\DataTable;
 class UsersDataTable extends DataTable
 {
 
-    public function dataTable()
-    {
-        return (new EloquentDataTable($this->query()))
-            ->editColumn('created_at', function (User $model) {
-                return date('d/m/Y H:i:s', strtotime($model->created_at));
-            }
-            )
-            ->addColumn('edit', function (User $model) {
-                return '<a href="users/' . $model->id . '/edit" 
+	public function dataTable()
+	{
+		return (new EloquentDataTable($this->query()))
+			->editColumn('created_at', function (User $model) {
+				return date('d/m/Y H:i:s', strtotime($model->created_at));
+			}
+			)
+			->addColumn('edit', function (User $model) {
+				return '<a href="users/' . $model->id . '/edit" 
                            class="btn btn-xs btn-primary center-block"> 
                             <i class="fa fa-pencil-square-o" 
                                aria-hidden="true"></i>
                         </a>';
-            }
-            )
-            ->editColumn('level', function (User $model) {
-                switch ($model->role) {
-                    case User::ADMIN:
-                        return '<label class="label label-sm label-danger center-block">Admin</label>';
-                    case User::DOCTOR:
-                        return '<label class="label label-sm label-info center-block">Médico</label>';
-                    case User::SECRETARY:
-                        return '<label class="label label-sm label-warning center-block">Secretário</label>';
-                    default:
-                        return '<label class="label label-sm label-default center-block">Desconhecido</label>';
-                }
-            }
-            )
-            ->escapeColumns([]);
-    }
+			}
+			)
+			->editColumn('role', function (User $model) {
+				switch ($model->role) {
+					case User::ADMIN:
+						return '<label class="label label-sm label-danger center-block">Admin</label>';
+					case User::DOCTOR:
+						return '<label class="label label-sm label-info center-block">Médico</label>';
+					case User::SECRETARY:
+						return '<label class="label label-sm label-warning center-block">Secretário</label>';
+					default:
+						return '<label class="label label-sm label-default center-block">Desconhecido</label>';
+				}
+			}
+			)
+			->escapeColumns([]);
+	}
 
-    /**
-     * Get the query object to be processed by dataTables.
-     *
-     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
-     */
-    public function query()
-    {
-        $query = User::query();
+	/**
+	 * Get the query object to be processed by dataTables.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
+	 */
+	public function query()
+	{
+		$query = User::query();
 
-        return $this->applyScopes($query);
-    }
+		return $this->applyScopes($query);
+	}
 
-    /**
-     * Optional method if you want to use html builder.
-     *
-     * @return \Yajra\DataTables\Html\Builder
-     */
-    public function html()
-    {
-        return $this->builder()
-                    ->columns($this->getColumns())
-                    ->setTableAttributes([
-                                             'class' => 'table table-bordered table-hover table-responsive table-full-width',
-                                         ]
-                    )
-                    ->parameters($this->getBuilderParameters())
-                    ->parameters([
-                                     'dom' => 'Blfrtip',
-                                     'responsive' => true,
-                                     'language' => ['url' => '/assets/global/plugins/datatables/DataTables-1.10.12/portuguese-brasil.json'],
-                                     'lengthMenu' => [
-                                         [
-                                             5,
-                                             10,
-                                             15,
-                                             20,
-                                             100
-                                         ],
-                                         [
-                                             5,
-                                             10,
-                                             15,
-                                             20,
-                                             100
-                                         ]
-                                     ],
-                                     'pageLength' => 10,
-                                     'buttons' => [
-                                         'create',
-                                         'export',
-                                         'print',
-                                         'reload',
-                                     ],
-                                 ]
-                    );
-    }
+	/**
+	 * Optional method if you want to use html builder.
+	 *
+	 * @return \Yajra\DataTables\Html\Builder
+	 */
+	public function html()
+	{
+		return $this->builder()
+		            ->columns($this->getColumns())
+		            ->setTableAttributes([
+				            'class' => 'table table-bordered table-hover table-responsive table-full-width',
+			            ]
+		            )
+		            ->parameters($this->getBuilderParameters())
+		            ->parameters([
+				            'dom'        => 'Blfrtip',
+				            'responsive' => true,
+				            'language'   => ['url' => '/assets/global/plugins/datatables/DataTables-1.10.12/portuguese-brasil.json'],
+				            'lengthMenu' => [
+					            [
+						            5,
+						            10,
+						            15,
+						            20,
+						            100
+					            ],
+					            [
+						            5,
+						            10,
+						            15,
+						            20,
+						            100
+					            ]
+				            ],
+				            'pageLength' => 10,
+				            'buttons'    => [
+					            'create',
+					            'export',
+					            'print',
+					            'reload',
+				            ],
+			            ]
+		            );
+	}
 
-    /**
-     * Get columns.
-     *
-     * @return array
-     */
-    protected function getColumns()
-    {
-        return [
-            'id',
-            'name' => ['title' => 'Nome'],
-            'email' => ['title' => 'E-mail'],
-            'address' => ['title' => 'Endereço'],
-            'neighborhood' => ['title' => 'Bairro'],
-            'number' => ['title' => 'Número'],
-            'level' => ['title' => 'Nível',
-                        'searchable' => false,
-                        'orderable' => false,
-                ],
-            'created_at' => ['title' => 'Data'],
-            'edit' => [
-                'title' => 'Editar',
-                'searchable' => false,
-                'orderable' => false
-            ]
-        ];
-    }
+	/**
+	 * Get columns.
+	 *
+	 * @return array
+	 */
+	protected function getColumns()
+	{
+		return [
+			'id',
+			'name'         => ['title' => 'Nome'],
+			'email'        => ['title' => 'E-mail'],
+			'address'      => ['title' => 'Endereço'],
+			'neighborhood' => ['title' => 'Bairro'],
+			'number'       => ['title' => 'Número'],
+			'role'         => ['title' => 'Nível'],
+			'created_at'   => ['title' => 'Data'],
+			'edit'         => [
+				'title'      => 'Editar',
+				'searchable' => false,
+				'orderable'  => false
+			]
+		];
+	}
 
-    /**
-     * Get filename for export.
-     *
-     * @return string
-     */
-    protected function filename()
-    {
-        return 'logs_' . time();
-    }
+	/**
+	 * Get filename for export.
+	 *
+	 * @return string
+	 */
+	protected function filename()
+	{
+		return 'users_' . time();
+	}
 }
--- resources/views/layouts/components/doctor/side-bar-doctor.blade.php
+++ resources/views/layouts/components/doctor/side-bar-doctor.blade.php
@@ -1,10 +1,10 @@
 <div class="page-sidebar-wrapper">
     <div class="page-sidebar navbar-collapse collapse">
-        <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-light"
+        <ul class="page-sidebar-menu page-header-fixed page-sidebar-menu-closed page-sidebar-menu-light hidden-sm hidden-xs"
             data-keep-expanded="false"
             data-auto-scroll="true"
             data-slide-speed="200"
-            style="padding-top: 20px">
+            style="padding-top: 10px">
             <li class="sidebar-toggler-wrapper hide">
                 <div class="sidebar-toggler">
                     <span></span>
@@ -34,14 +34,16 @@
             <li class="nav-item start {{ Request::is('profile') ? 'active' : '' }}">
                 <a href="{{ url('/profile') }}"
                    class="nav-link ">
-                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <i class="fa fa-home"
+                       aria-hidden="true"></i>
                     <span class="title">Início</span>
                 </a>
             </li>
             <li class="nav-item start {{ Request::is('doctor/patients') ? 'active' : '' }} {{ Request::is('doctor/patients/create') ? 'active' : '' }}">
                 <a href="javascript:;"
                    class="nav-link nav-toggle">
-                    <i class="fa fa-wheelchair" aria-hidden="true"></i>
+                    <i class="fa fa-wheelchair"
+                       aria-hidden="true"></i>
                     <span class="title">Paciente</span>
                     <span class="arrow"></span>
                 </a>
@@ -49,14 +51,16 @@
                     <li class="nav-item start {{ Request::is('doctor/patients/create') ? 'active' : '' }}">
                         <a href="{{ url('/doctor/patients/create') }}"
                            class="nav-link ">
-                            <i class="fa fa-plus-square" aria-hidden="true"></i>
+                            <i class="fa fa-plus-square"
+                               aria-hidden="true"></i>
                             <span class="title">Adicionar</span>
                         </a>
                     </li>
                     <li class="nav-item start {{ Request::is('doctor/patients') ? 'active' : '' }}">
                         <a href="{{ url('/doctor/patients') }}"
                            class="nav-link ">
-                            <i class="fa fa-list" aria-hidden="true"></i>
+                            <i class="fa fa-list"
+                               aria-hidden="true"></i>
                             <span class="title">Listar</span>
                         </a>
                     </li>
@@ -65,7 +69,8 @@
             <li class="nav-item start {{ Request::is('doctor/schedules') ? 'active' : '' }} {{ Request::is('doctor/schedules/calendar') ? 'active' : '' }}">
                 <a href="javascript:;"
                    class="nav-link nav-toggle">
-                    <i class="fa fa-address-book" aria-hidden="true"></i>
+                    <i class="fa fa-address-book"
+                       aria-hidden="true"></i>
                     <span class="title">Agenda e Compromissos</span>
                     <span class="arrow"></span>
                 </a>
@@ -73,14 +78,16 @@
                     <li class="nav-item start {{ Request::is('doctor/schedules/calendar') ? 'active' : '' }}">
                         <a href="{{ url('/doctor/schedules/calendar') }}"
                            class="nav-link ">
-                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                            <i class="fa fa-calendar"
+                               aria-hidden="true"></i>
                             <span class="title">Calendário</span>
                         </a>
                     </li>
                     <li class="nav-item start {{ Request::is('doctor/schedules') ? 'active' : '' }}">
                         <a href="{{ url('/doctor/schedules') }}"
                            class="nav-link ">
-                            <i class="fa fa-table" aria-hidden="true"></i>
+                            <i class="fa fa-table"
+                               aria-hidden="true"></i>
                             <span class="title">Tabela</span>
                         </a>
                     </li>
