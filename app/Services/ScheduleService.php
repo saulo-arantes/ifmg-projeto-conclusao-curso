79f0264bfdc90a6c95ef85084b05a2209a14d4e3
Saulo Vinícius <saulovinicius987@gmail.com>
2017-10-11 09:51:26 -0300

    Erros na lógica de virificação de agendamentos fixados.

--- app/Services/ScheduleService.php
+++ app/Services/ScheduleService.php
@@ -23,198 +23,220 @@ use Prettus\Validator\Exceptions\ValidatorException;
  * @since 04/08/2017
  * @package App\Services
  */
-class ScheduleService
-{
+class ScheduleService {
 
-    protected $repository;
-    protected $validator;
+	protected $repository;
+	protected $validator;
 
-    /**
-     * ScheduleService constructor.
-     *
-     * @param ScheduleRepository $repository
-     * @param ScheduleValidator $validator
-     */
-    public function __construct(ScheduleRepository $repository, ScheduleValidator $validator)
-    {
-        $this->repository = $repository;
-        $this->validator = $validator;
-    }
+	/**
+	 * ScheduleService constructor.
+	 *
+	 * @param ScheduleRepository $repository
+	 * @param ScheduleValidator $validator
+	 */
+	public function __construct(ScheduleRepository $repository, ScheduleValidator $validator) {
+		$this->repository = $repository;
+		$this->validator  = $validator;
+	}
 
-    public function store(array $data)
-    {
+	public function store(array $data) {
 
-        $startAt = Carbon::createFromFormat('d/m/Y H:i', $data['start_at']);
-        $finishAt = Carbon::createFromFormat('d/m/Y H:i', $data['finish_at']);
+		$startAt  = Carbon::createFromFormat('d/m/Y H:i', $data['start_at']);
+		$finishAt = Carbon::createFromFormat('d/m/Y H:i', $data['finish_at']);
 
-        $data['start_at'] = $startAt;
-        $data['finish_at'] = $finishAt;
+		$data['start_at']  = $startAt;
+		$data['finish_at'] = $finishAt;
 
-        if ($data['finish_at'] < $data['start_at']) {
-            return [
-                'error' => true,
-                'message' => 'Opeação inválida. 
+		if ($data['finish_at'] < $data['start_at']) {
+			return [
+				'error'   => true,
+				'message' => 'Opeação inválida. 
 				A data de Término não pode ser menor que a data de Início.'
-            ];
-        }
+			];
+		}
 
-        $data['type'] = session('type');
+		if ($data['start_at'] < Carbon::now()) {
+			return [
+				'error'   => true,
+				'message' => 'Opeação inválida. 
+				Defina uma data maior ou igual a data de agora.'
+			];
+		}
 
-        if ($data['type'] == 1 && User::isDoctor()) {
-            $doctors = Doctor::all();
-            foreach ($doctors as $doctor) {
-                if  (Auth::id() == $doctor['user_id']) {
-                    $data['doctor_id'] = $doctor['id'];
-                }
-            }
-        }
+		$data['type'] = session('type');
 
-        $schedules = Schedule::all();
+		if ($data['type'] == 1 && User::isDoctor()) {
+			$doctors = Doctor::all();
+			foreach ($doctors as $doctor) {
+				if (Auth::id() == $doctor['user_id']) {
+					$data['doctor_id'] = $doctor['id'];
+				}
+			}
+		}
 
-        if ($data['type'] == 0) {
-            foreach ($schedules as $schedule) {
-                if ($data['doctor_id'] = $schedule['doctor_id']) {
-                    if ($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) {
-                        if ($data['status'] != Schedule::ACCOMPLISHED && $data['status'] != Schedule::CANCELED) {
-                            return [
-                                'error' => true,
-                                'message' => 'Opeação inválida. 
+		$schedules = Schedule::all();
+
+		if ($data['type'] == 0) {
+			foreach ($schedules as $schedule) {
+				if ($data['doctor_id'] == $schedule['doctor_id']) {
+					if (($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) ||
+					    ($data['start_at'] <= $schedule['start_at'] && $schedule['start_at'] <= $data['finish_at']) ||
+					    ($data['start_at'] < $schedule['start_at'] && $data['finish_at'] > $schedule['finish_at'])) {
+						if ($data['status'] != Schedule::ACCOMPLISHED && $data['status'] != Schedule::CANCELED) {
+							return [
+								'error'   => true,
+								'message' => 'Opeação inválida. 
                                     Já existe um compromisso neste horário para esse médico.'
-                            ];
-                        }
-                    }
-                }
-            }
-        } elseif ($data['type'] == 1) {
-            foreach ($schedules as $schedule) {
-                if ($data['doctor_id'] == $schedule['doctor_id']) {
-                    if ($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) {
-                        return [
-                            'error' => true,
-                            'message' => 'Opeação inválida. 
+							];
+
+						}
+					}
+				}
+			}
+		} elseif ($data['type'] == 1) {
+			foreach ($schedules as $schedule) {
+				if ($data['doctor_id'] == $schedule['doctor_id']) {
+					if (($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) ||
+					    ($data['start_at'] <= $schedule['start_at'] && $schedule['start_at'] <= $data['finish_at']) ||
+					    ($data['start_at'] < $schedule['start_at'] && $data['finish_at'] > $schedule['finish_at'])) {
+						return [
+							'error'   => true,
+							'message' => 'Opeação inválida. 
 							Já existe um compromisso neste horário.'
-                        ];
-                    }
-                }
-            }
-        }
+						];
+					}
+				}
+			}
+		}
 
-        try {
+		try {
 
-            $this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_CREATE);
+			$this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_CREATE);
 
-            $schedule = $this->repository->create($data);
+			$schedule = $this->repository->create($data);
 
-            return $schedule['data']['id'];
+			return $schedule['data']['id'];
 
-        } catch (ValidatorException $exception) {
+		} catch (ValidatorException $exception) {
 
-            Notification::send(User::allAdmins(),
-                new ValidatorExceptionNotification($exception->getFile(), $exception->getLine(),
-                    $exception->getMessageBag()->first()));
+			Notification::send(User::allAdmins(),
+				new ValidatorExceptionNotification($exception->getFile(), $exception->getLine(),
+					$exception->getMessageBag()->first()));
 
-            return [
-                'error' => true,
-                'message' => $exception->getMessageBag()->first()
-            ];
-        } catch (\Exception $exception) {
+			return [
+				'error'   => true,
+				'message' => $exception->getMessageBag()->first()
+			];
+		} catch (\Exception $exception) {
 
-            Notification::send(User::allAdmins(),
-                new ExceptionNotification($exception->getFile(), $exception->getLine(),
-                    $exception->getMessage()));
+			Notification::send(User::allAdmins(),
+				new ExceptionNotification($exception->getFile(), $exception->getLine(),
+					$exception->getMessage()));
 
-            return [
-                'error' => true,
-                'message' => 'Ocorreu um erro ao adicionar agendamento.'
-            ];
-        }
-    }
+			return [
+				'error'   => true,
+				'message' => 'Ocorreu um erro ao adicionar agendamento.'
+			];
+		}
+	}
 
-    public function update(array $data, $id)
-    {
+	public function update(array $data, $id) {
 
-        $startAt = Carbon::createFromFormat('d/m/Y H:i', $data['start_at']);
-        $finishAt = Carbon::createFromFormat('d/m/Y H:i', $data['finish_at']);
+		$startAt  = Carbon::createFromFormat('d/m/Y H:i', $data['start_at']);
+		$finishAt = Carbon::createFromFormat('d/m/Y H:i', $data['finish_at']);
 
-        $data['start_at'] = $startAt;
-        $data['finish_at'] = $finishAt;
+		$data['start_at']  = $startAt;
+		$data['finish_at'] = $finishAt;
 
-        if ($data['finish_at'] < $data['start_at']) {
-            return [
-                'error' => true,
-                'message' => 'Opeação inválida. 
+		if ($data['finish_at'] < $data['start_at']) {
+			return [
+				'error'   => true,
+				'message' => 'Opeação inválida. 
 				A data de Término não pode ser menor que a data de Início.'
-            ];
-        }
+			];
+		}
 
-        $data['type'] = session('type');
+		if ($data['start_at'] < Carbon::now()) {
+			return [
+				'error'   => true,
+				'message' => 'Opeação inválida. 
+				Defina uma data maior ou igual a data de agora.'
+			];
+		}
 
-        if ($data['type'] == 1 && User::isDoctor()) {
-            $doctors = Doctor::all();
-            foreach ($doctors as $doctor) {
-                if  (Auth::id() == $doctor['user_id']) {
-                    $data['doctor_id'] = $doctor['id'];
-                }
-            }
-        }
+		$data['type'] = session('type');
 
-        $schedules = Schedule::all();
+		if ($data['type'] == 1 && User::isDoctor()) {
+			$doctors = Doctor::all();
+			foreach ($doctors as $doctor) {
+				if (Auth::id() == $doctor['user_id']) {
+					$data['doctor_id'] = $doctor['id'];
+				}
+			}
+		}
 
-        if ($data['type'] == 0) {
-            foreach ($schedules as $schedule) {
-                if ($data['doctor_id'] == $schedule['doctor_id']) {
-                    if ($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) {
-                        if ($data['status'] != Schedule::ACCOMPLISHED && $data['status'] != Schedule::CANCELED) {
-                            return [
-                                'error' => true,
-                                'message' => 'Opeação inválida. 
+		$schedules = Schedule::all();
+
+		if ($data['type'] == 0) {
+			foreach ($schedules as $schedule) {
+				if ($data['doctor_id'] == $schedule['doctor_id']) {
+					if (($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) ||
+					    ($data['start_at'] <= $schedule['start_at'] && $schedule['start_at'] <= $data['finish_at']) ||
+					    ($data['start_at'] < $schedule['start_at'] && $data['finish_at'] > $schedule['finish_at'])) {
+						if ($data['status'] != Schedule::ACCOMPLISHED && $data['status'] != Schedule::CANCELED) {
+							return [
+								'error'   => true,
+								'message' => 'Opeação inválida. 
                                     Já existe um compromisso neste horário para esse médico.'
-                            ];
-                        }
-                    }
-                }
-            }
-        } elseif ($data['type'] == 1) {
-            foreach ($schedules as $schedule) {
-                if ($data['doctor_id'] = $schedule['doctor_id']) {
-                    if ($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) {
-                        return [
-                            'error' => true,
-                            'message' => 'Opeação inválida. 
+							];
+
+						}
+					}
+				}
+			}
+		} elseif ($data['type'] == 1) {
+			foreach ($schedules as $schedule) {
+				if ($data['doctor_id'] == $schedule['doctor_id']) {
+					if (($data['start_at'] >= $schedule['start_at'] && $data['start_at'] < $schedule['finish_at']) ||
+					    ($data['start_at'] <= $schedule['start_at'] && $schedule['start_at'] <= $data['finish_at']) ||
+					    ($data['start_at'] < $schedule['start_at'] && $data['finish_at'] > $schedule['finish_at'])) {
+						return [
+							'error'   => true,
+							'message' => 'Opeação inválida. 
 							Já existe um compromisso neste horário.'
-                        ];
-                    }
-                }
-            }
-        }
+						];
+					}
+				}
+			}
+		}
 
-        try {
+		try {
 
-            $this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_UPDATE);
-            $this->repository->update($data, $id);
+			$this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_UPDATE);
+			$this->repository->update($data, $id);
 
-            return true;
-        } catch (ValidatorException $exception) {
+			return true;
+		} catch (ValidatorException $exception) {
 
-            Notification::send(User::allAdmins(),
-                new ValidatorExceptionNotification($exception->getFile(), $exception->getLine(),
-                    $exception->getMessageBag()->first()));
+			Notification::send(User::allAdmins(),
+				new ValidatorExceptionNotification($exception->getFile(), $exception->getLine(),
+					$exception->getMessageBag()->first()));
 
-            return [
-                'error' => true,
-                'message' => $exception->getMessageBag()->first()
-            ];
-        } catch (\Exception $exception) {
+			return [
+				'error'   => true,
+				'message' => $exception->getMessageBag()->first()
+			];
+		} catch (\Exception $exception) {
 
-            Notification::send(User::allAdmins(),
-                new ExceptionNotification($exception->getFile(), $exception->getLine(),
-                    $exception->getMessage()));
+			Notification::send(User::allAdmins(),
+				new ExceptionNotification($exception->getFile(), $exception->getLine(),
+					$exception->getMessage()));
 
-            return [
-                'error' => true,
-                'message' => 'Ocorreu um erro ao editar o agendamento.'
-            ];
+			return [
+				'error'   => true,
+				'message' => 'Ocorreu um erro ao editar o agendamento.'
+			];
 
-        }
-    }
+		}
+	}
 }
